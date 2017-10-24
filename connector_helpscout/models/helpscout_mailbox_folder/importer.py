7c09a6136701d8be67623c3924adf0e067625da3
Brenton Hughes <brent.hughes@laslabs.com>
2017-10-23 17:25:05 -0700

    [IMP] connector_helpscout: Improve folder naming
    * Make name more useful if external folder name is "Mine" by
      appending user name

--- connector_helpscout/models/helpscout_mailbox_folder/importer.py
+++ connector_helpscout/models/helpscout_mailbox_folder/importer.py
@@ -14,20 +14,35 @@ class HelpScoutMailboxFolderImportMapper(Component):
     _inherit = 'helpscout.import.mapper'
     _apply_on = 'helpscout.mailbox.folder'
 
-    direct = [('name', 'name'),
-              ('type', 'helpscout_type'),
+    direct = [('type', 'helpscout_type'),
               ('modified_at', 'backend_date_modified'),
               ]
+
+    def _get_user_name(self, user_id):
+        user = self.env['helpscout.user'].search([
+            ('external_id', '=', user_id),
+        ]).odoo_id
+        return user.name
+
+    def _internal_name(self, record):
+        if record.type == 'mine':
+            user_name = self._get_user_name(record.user_id)
+            return '%s (%s)' % (record.name, user_name)
+        return record.name
 
     @mapping
     @only_create
     def id(self, record):
         """Searches helpscout.mailbox.folder records for matching name"""
         folder = self.env['helpscout.mailbox.folder'].search([
-            ('name', '=', record.name),
+            ('name', '=', self._internal_name(self, record)),
         ])
         if folder:
             return {'id': folder.id}
+
+    @mapping
+    def name(self, record):
+        return {'name': self._internal_name(self, record)}
 
 
 class HelpScoutMailboxFolderImporter(Component):
