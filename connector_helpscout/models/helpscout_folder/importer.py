5618ca372ac3b739ed07052cd9b732df6880ea32
Brenton Hughes <brent.hughes@laslabs.com>
2017-11-02 17:35:05 -0700

    [IMP] connector_helpscout: Fix import formatting

--- connector_helpscout/models/helpscout_folder/importer.py
+++ connector_helpscout/models/helpscout_folder/importer.py
@@ -3,7 +3,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.addons.component.core import Component
-from odoo.addons.connector.components.mapper import mapping, only_create
+from odoo.addons.connector.components.mapper import (mapping,
+                                                     only_create,
+                                                     )
 
 
 class HelpScoutFolderImportMapper(Component):
